e7f109eabbbdac874a2fe3ee8f0752a35a2ca223
luca <lucamonty00@gmail.com>
2025-08-29 17:11:22 +0200

    observability: Completed math service

---
+++ observability/math-otlp/math-service/src/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from prometheus_fastapi_instrumentator import Instrumentator
+import os, uvicorn
+from controller.math import get_divisors_route
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"]
+)
+
+Instrumentator().instrument(app).expose(app)
+
+app.include_router(get_divisors_route())
+
+ip = os.getenv("HOST", "127.0.0.1")
+port = int(os.getenv("PORT", "8080"))
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=ip, port=port)
