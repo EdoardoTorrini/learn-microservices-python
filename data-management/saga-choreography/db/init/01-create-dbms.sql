96621a5fb24c607f6c520518c23d3a79429eb244
Menoz2000 <menozzi2000@gmail.com>
2025-08-29 17:36:29 +0200

    compose funzionante per conductor e workflow da utilizzare

--- data-management/saga-choreography/db/init/01-create-dbms.sql
+++
@@ -1,6 +0,0 @@
--- Creazione dei tre database per i microservizi
-CREATE DATABASE payment_db;
-CREATE DATABASE inventory_db;
--- order_db è creato via POSTGRES_DB
-
--- Nota: le tabelle e i seed vengono creati dai servizi all'avvio
